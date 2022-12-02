372b9f281f6c273e0ada22ea49dbd80853af34ec
dentsucreativestudio <119412444+dentsucreativestudio@users.noreply.github.com>
2022-12-02 16:17:02 +0100

    Update vertical-images-settings.js

--- data/01-wijnegem/vertical-images-settings.js
+++ data/01-wijnegem/vertical-images-settings.js
@@ -1,12 +1,8 @@
 var settings = [
   { image: 'https://dentsucreativestudio.github.io/canda-dooh/images/Brand_Xmas_02-Shooting_1080x1920px_Anna.jpg'},  
-  
   { image: 'https://dentsucreativestudio.github.io/canda-dooh/images/01-wijnegem/Brand_Xmas_Shooting_1080x1920px_Anna.jpg'},
   { image: 'https://dentsucreativestudio.github.io/canda-dooh/images/01-wijnegem/Brand_Xmas_Shooting_1080x1920px_Eveline.jpg'},
   { image: 'https://dentsucreativestudio.github.io/canda-dooh/images/01-wijnegem/Brand_Xmas_Shooting_1080x1920px_Nancy.jpg'},
-  
-  { image: 'https://dentsucreativestudio.github.io/canda-dooh/images/01-wijnegem/Brand_Xmas_Shooting_1080x1920px_Anna.jpg'},
-  { image: 'https://dentsucreativestudio.github.io/canda-dooh/images/01-wijnegem/Brand_Xmas_Shooting_1080x1920px_Eveline.jpg'},
-  { image: 'https://dentsucreativestudio.github.io/canda-dooh/images/01-wijnegem/Brand_Xmas_Shooting_1080x1920px_Nancy.jpg'},
+
 ];
 
