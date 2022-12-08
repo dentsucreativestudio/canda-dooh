339a7f37e8d2236118cc54d3e756647fd60e85d3
dentsucreativestudio <119412444+dentsucreativestudio@users.noreply.github.com>
2022-12-08 12:52:16 +0100

    Add files via upload

--- data/05-belle-etoile/vertical-images-settings.js
+++ data/05-belle-etoile/vertical-images-settings.js
@@ -1,5 +1,5 @@
 var settings = [
-  { image: 'https://dentsucreativestudio.github.io/canda-dooh/images/04-waasland/Brand_Xmas_Shooting_1080x1920px_Anna.jpg'},
-  { image: 'https://dentsucreativestudio.github.io/canda-dooh/images/04-waasland/Brand_Xmas_Shooting_1080x1920px_Eveline.jpg'},
-  { image: 'https://dentsucreativestudio.github.io/canda-dooh/images/04-waasland/Brand_Xmas_Shooting_1080x1920px_Nancy.jpg'},
+  { image: 'https://dentsucreativestudio.github.io/canda-dooh/images/05-belle-etoile/Brand_Xmas_Shooting_1080x1920px_Anna.jpg'},
+  { image: 'https://dentsucreativestudio.github.io/canda-dooh/images/05-belle-etoile/Brand_Xmas_Shooting_1080x1920px_Eveline.jpg'},
+  { image: 'https://dentsucreativestudio.github.io/canda-dooh/images/05-belle-etoile/Brand_Xmas_Shooting_1080x1920px_Nancy.jpg'},
 ];
