08f3ac9333784ada60d38401f845d8bfe737cec5
dentsucreativestudio <119412444+dentsucreativestudio@users.noreply.github.com>
2022-12-10 11:44:41 +0100

    Update vertical-images-settings.js

--- data/03-belle-ile/vertical-images-settings.js
+++ data/03-belle-ile/vertical-images-settings.js
@@ -1,6 +1,5 @@
 var settings = [
-  { image: 'https://dentsucreativestudio.github.io/canda-dooh/images/03-belle-ile/Brand_Xmas_Shooting_1080x1920px_Anna.jpg'},
-  { image: 'https://dentsucreativestudio.github.io/canda-dooh/images/03-belle-ile/Brand_Xmas_Shooting_1080x1920px_Eveline.jpg'},
-  { image: 'https://dentsucreativestudio.github.io/canda-dooh/images/03-belle-ile/Brand_Xmas_Shooting_1080x1920px_Nancy.jpg'},
   { image: 'https://dentsucreativestudio.github.io/canda-dooh/images/03-belle-ile/Brand_Xmas_Shooting_1080x1920px_laraliegeois1.jpg'}
+  { image: 'https://dentsucreativestudio.github.io/canda-dooh/images/03-belle-ile/Brand_Xmas_Shooting_1080x1920px_bernardsireuil2.jpg'}
+ 
 ];
