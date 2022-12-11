7f9b82965592b193ee5978babcdc1fc7fdac6ce1
dentsucreativestudio <119412444+dentsucreativestudio@users.noreply.github.com>
2022-12-11 11:41:04 +0100

    Update vertical-images-settings.js

--- data/05-belle-etoile/vertical-images-settings.js
+++ data/05-belle-etoile/vertical-images-settings.js
@@ -1,4 +1,5 @@
 var settings = [
+  { image: 'https://dentsucreativestudio.github.io/canda-dooh/images/05-belle-etoile/Brand_Xmas_Shooting_1080x1920px_Giana1.jpg'},
   { image: 'https://dentsucreativestudio.github.io/canda-dooh/images/05-belle-etoile/Brand_Xmas_Shooting_1080x1920px_Anna.jpg'},
   { image: 'https://dentsucreativestudio.github.io/canda-dooh/images/05-belle-etoile/Brand_Xmas_Shooting_1080x1920px_Eveline.jpg'},
   { image: 'https://dentsucreativestudio.github.io/canda-dooh/images/05-belle-etoile/Brand_Xmas_Shooting_1080x1920px_Nancy.jpg'},
