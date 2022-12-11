455ec05c3215ef4fdbf19fab3c27eb7fc17ecde6
dentsucreativestudio <119412444+dentsucreativestudio@users.noreply.github.com>
2022-12-11 12:05:48 +0100

    Update vertical-images-settings.js

--- data/05-belle-etoile/vertical-images-settings.js
+++ data/05-belle-etoile/vertical-images-settings.js
@@ -1,5 +1,7 @@
 var settings = [
   { image: 'https://dentsucreativestudio.github.io/canda-dooh/images/05-belle-etoile/Brand_Xmas_Shooting_1080x1920px_Giana1.jpg'},
+  { image: 'https://dentsucreativestudio.github.io/canda-dooh/images/05-belle-etoile/Brand_Xmas_Shooting_1080x1920px_Melanie2.jpg'},
+  { image: 'https://dentsucreativestudio.github.io/canda-dooh/images/05-belle-etoile/Brand_Xmas_Shooting_1080x1920px_MariaHelena3.jpg'},
   { image: 'https://dentsucreativestudio.github.io/canda-dooh/images/05-belle-etoile/Brand_Xmas_Shooting_1080x1920px_Anna.jpg'},
   { image: 'https://dentsucreativestudio.github.io/canda-dooh/images/05-belle-etoile/Brand_Xmas_Shooting_1080x1920px_Eveline.jpg'},
   { image: 'https://dentsucreativestudio.github.io/canda-dooh/images/05-belle-etoile/Brand_Xmas_Shooting_1080x1920px_Nancy.jpg'},
