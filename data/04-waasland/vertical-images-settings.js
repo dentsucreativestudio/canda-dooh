4060e83d166f3ccd07b95c560a3897d9de8b6299
dentsucreativestudio <119412444+dentsucreativestudio@users.noreply.github.com>
2022-12-17 13:08:50 +0100

    Update vertical-images-settings.js

--- data/04-waasland/vertical-images-settings.js
+++ data/04-waasland/vertical-images-settings.js
@@ -1,4 +1,7 @@
 var settings = [
+   { image: 'https://dentsucreativestudio.github.io/canda-dooh/images/04-waasland/Brand_Xmas_Shooting_Ajdarewit & Gashi9_.jpg'},
+   { image: 'https://dentsucreativestudio.github.io/canda-dooh/images/04-waasland/Brand_Xmas_Shooting_LouisePauline8x_.jpg'},
+   { image: 'https://dentsucreativestudio.github.io/canda-dooh/images/04-waasland/Brand_Xmas_Shooting_384x960px_.jpg'},
   { image: 'https://dentsucreativestudio.github.io/canda-dooh/images/04-waasland/Brand_Xmas_Shooting_384x960px_.jpg'},
   { image: 'https://dentsucreativestudio.github.io/canda-dooh/images/04-waasland/Brand_Xmas_Shooting_384x960px_-2.jpg'},
   { image: 'https://dentsucreativestudio.github.io/canda-dooh/images/04-waasland/Brand_Xmas_Shooting_TianaEmmaYluna3.jpg'},
